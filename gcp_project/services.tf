68f3973a629064382bdbb132812fd067d92fed8c
Anders Pearson <anders@thraxil.org>
2019-09-06 10:28:08 +0100

    project labels

--- gcp_project/services.tf
+++ gcp_project/services.tf
@@ -1,5 +1,6 @@
 resource "google_project_service" "service" {
-  count   = "${length(var.services)}"
-  project = "${var.project_id}"
-  service = "${element(var.services, count.index)}"
+  count                      = "${length(var.services)}"
+  project                    = "${var.project_id}"
+  service                    = "${element(var.services, count.index)}"
+  disable_dependent_services = true
 }
