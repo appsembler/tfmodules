9220a727f68e8173f43b0c180cb1f790ea9f4f82
Anders Pearson <anders@thraxil.org>
2020-02-04 15:31:41 +0000

    simplify updating services

--- gcp_project/services.tf
+++ gcp_project/services.tf
@@ -2,5 +2,7 @@ resource "google_project_service" "service" {
   count                      = "${length(var.services)}"
   project                    = "${var.project_id}"
   service                    = "${element(var.services, count.index)}"
-  disable_dependent_services = true
+
+  disable_dependent_services = false
+  disable_on_destroy = false
 }
