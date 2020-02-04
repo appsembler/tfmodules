69a25569a4a7b2a3c4b81fe0c84b84951ba14e27
Anders Pearson <anders@thraxil.org>
2020-02-04 16:08:21 +0000

    fix project id reference

--- gcp_project/ssh_keys.tf
+++ gcp_project/ssh_keys.tf
@@ -1,5 +1,5 @@
 resource "google_compute_project_metadata_item" "ssh-keys" {
   key     = "ssh-keys"
   value   = "${var.ssh_keys}"
-  project = "${google_project.project.id}"
+  project = "${var.project_id}"
 }
