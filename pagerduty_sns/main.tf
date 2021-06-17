3e400c256fd9b64aac7a20557e74dbaba1cc40f9
Anders Pearson <anders@thraxil.org>
2021-06-17 09:55:23 +0100

    pagerduty-sns: remove interpolation
    no longer needed

--- pagerduty_sns/main.tf
+++ pagerduty_sns/main.tf
@@ -1,10 +1,10 @@
 resource "aws_sns_topic" "topic" {
-  name = "${var.name}"
+  name = var.name
 }
 
 resource "aws_sns_topic_subscription" "subscription" {
-  topic_arn              = "${aws_sns_topic.topic.arn}"
+  topic_arn              = aws_sns_topic.topic.arn
   protocol               = "https"
-  endpoint               = "${var.pagerduty_integration_url}"
+  endpoint               = var.pagerduty_integration_url
   endpoint_auto_confirms = true
 }
