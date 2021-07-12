8d7601167a5405eefd1c95f2a261b8f6b1bb3955
Anders Pearson <anders@thraxil.org>
2021-07-12 11:05:55 +0100

    support folder_id on gcp_project

--- gcp_project/project.tf
+++ gcp_project/project.tf
@@ -2,6 +2,7 @@ resource "google_project" "project" {
   name            = var.name
   project_id      = var.project_id
   org_id          = var.org_id
+  folder_id       = var.folder_id
   billing_account = var.billing_account
   labels          = var.labels
 }
